a3ca9f18a66218c80bc0c2f828d2677851e02bb4
Vinyzada <vinyescolS@gmail.com>
2024-02-23 15:17:47 -0300

    Adicionado a função de reduzir e reniciar contador

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,21 @@
 
   import { ref } from 'vue'
   const contador = ref(0)
-  
+ function Subtrair(){
+  contador.value>0 ? contador.value-- : null
+ }
+
 
 </script>
 
 <template>
   <button @click="contador++">+</button>
-  <button @click="contador--">-</button>
+  <button @click="Subtrair()">-</button>
+  <button @click="contador=0">Reset</button>
+
   <p>Contador: {{ contador }}</p>
 </template>
 
 <style scoped>
+
 </style>
